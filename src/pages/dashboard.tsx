15af6dbe6fa7ea255a9fa34c5b6c5fb982bcd060
software groups <admin@sfgroups.com>
2022-08-07 22:02:14 -0400

    added  react componentes

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,27 @@
-const DashboardPage = () => <div>Dashboard</div>;
+import * as React from "react";
 
-export default DashboardPage;
+import UserComponent from './ui/UserComponent'
+import CustomizedTimeline from "./ui/CustomizedTimeline"
+import Hello from './ui/Hello'
+
+let Logo ="https://www.sfgroups.com/logos/sfg.gif";
+export default class FirstComponent extends React.Component <{}> {
+  render() {
+    return (
+      <div>
+        <CustomizedTimeline/>
+        <h3>A Simple React Component Example with Typescript</h3>
+        <div>
+          <img height="50" src={Logo}  alt="logo"/> 
+        </div>
+        <p>This component shows the Logrocket logo.</p>
+        <p>For more info on Logrocket, please visit https://logrocket.com </p>
+      
+        <UserComponent name="John Doe" age={26} address="11 Rodeo Dr., Columbus, GA 02110" dob={new Date()} />
+
+        <hr/>
+        <Hello name="Rishi" enthusiasmLevel={2} />
+      </div>
+    );
+  }
+}
